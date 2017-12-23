ac5a6e0177e72823ee63eb0350c1bf3270fd1a2d
f.hilitski@gmail.com <f.hilitski@gmail.com>
2017-12-23 10:28:57 -0500

    adding support for SERVER_HTML_DIR environment configvar

--- node-test/node-test-5.js
+++ node-test/node-test-5.js
@@ -3,10 +3,14 @@ const fs = require('fs');
 
 const hostname = '10.0.0.85';
 const port = 3000;
+const server_html_dir = process.env.SERVER_HTML_DIR;
+/*
+ export SERVER_HTML_DIR to the environment - this is a root dir with HTLM pages to serve
+*/
 
 const server = http.createServer();
 
-server.on('request', (req, res) => {
+server.on('request', function(req, res){
   const header_json = {'Content-Type':'text/html','X-Powered-By':'sleepless-nights'};
   res.writeHead(200, header_json); 
   
@@ -19,6 +23,7 @@ server.on('request', (req, res) => {
   else if (url == '/start.html?') {
       fname = 'action.html';
   } else {};
+  fname = server_html_dir + '/' + fname;
 
   fs.readFile(fname, function(error, contents){
      res.write(contents);
@@ -46,4 +51,5 @@ server.on('request', (req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server HTML root dir: ${server_html_dir}`);
 });
