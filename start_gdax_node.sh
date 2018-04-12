d62d99d06ab6f09e62d121ce6f34daaed41dd6b6
f.hilitski@gmail.com <f.hilitski@gmail.com>
2018-04-11 21:48:40 -0400

    start_gdax_node: checking for wlan ip first, then ethernet, and defaulting to localhost if both are not present

--- start_gdax_node.sh
+++ start_gdax_node.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+# this should be launched from the root directory that contains html, log and gdax-node sub-dirs
 
 export SERVER_HTML_DIR=$(pwd)/html
 export SERVER_PORT=3000
-#export SERVER_ADDRESS=$(sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
-export SERVER_ADDRESS='localhost'
+
+# determine ip address - check if wlan
+SERVER_ADDRESS=$(/sbin/ifconfig wlan0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+if [[ -z $SERVER_ADDRESS ]]
+then
+  SERVER_ADDRESS=$(/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+fi
+if [[ -z $SERVER_ADDRESS ]]
+then
+   SERVER_ADDRESS='localhost'
+fi
+export SERVER_ADDRESS
+
 date=$(date +%m%d%y)
 ./node_bg.sh gdax-node/source/gdax-node.js ./log/stdout-$date.txt ./log/error-$date.txt
